7b3dc7b80203b4bfa3bd27c697159fc451e0f660
SergeyShidlovsky <shedlovskys@gmail.com>
2022-11-26 18:59:10 +0300

    Added gitignore

--- src/main/java/runner/TestFrame.java
+++ src/main/java/runner/TestFrame.java
@@ -11,206 +11,64 @@ import javax.swing.*;
 //See changelog.txt for qny questions
 
 public class TestFrame extends JFrame {
-    static int i = 0;
+    int quantity = 9;
+    private Timer timer;
+    protected Font font;
+    protected Font labelFont;
+    protected JLabel[] statusLabel;
+    protected JTabbedPane tabbedPane;
 
     public TestFrame() {
 
-        super("Тестовое окно");
+        super("Tool For QA Engineers");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        final Font font = new Font("Verdana", Font.LAYOUT_LEFT_TO_RIGHT, 10);
-        JTabbedPane tabbedPane = new JTabbedPane();
+        //Font of Buttons and Labels
+        font = new Font("Verdana", Font.LAYOUT_LEFT_TO_RIGHT, 10);
+        labelFont  = new Font("Verdana", Font.LAYOUT_LEFT_TO_RIGHT, 20);
+
+        //Creating tabbed pane with font
+        tabbedPane = new JTabbedPane();
         tabbedPane.setFont(font);
 
-        class Exec1 extends JPanel{
-            Exec1 () {
-
-                // MyKey listener = new MyKey();
-                // addKeyListener(listener);
-                setFocusable(true);
-
-                //Configure visual settings of Button1
-                final JButton button1 = new JButton("Open Appdata [1]");
-                button1.setFont(font);
-                button1.setVisible(true);
-                button1.setToolTipText("Opens %Appata% folder");
-                button1.setSize(313, 110);
-                add(button1);
-
-                //Configure visual setting of Button2
-                final JButton button2 = new JButton("Open logs [2]");
-                button2.setFont(font);
-                button2.setVisible(true);
-                button1.setToolTipText("Opens CUE Log folder");
-                button2.setSize(313, 110);
-                add(button2);
-
-                //Configure visual settings of Button3
-                final JButton button3 = new JButton("Clear Appdata [3]");
-                button1.setFont(font);
-                button1.setVisible(true);
-                button1.setToolTipText("Delete CUE %Appata% folder");
-                button1.setSize(313, 110);
-                add(button3);
-
-                //Configure visual setting of Button4
-                final JButton button4 = new JButton("Clear logs [4]");
-                button2.setFont(font);
-                button2.setVisible(true);
-                button1.setToolTipText("Delete CUE Log folder");
-                button2.setSize(313, 110);
-                add(button4);
-
-                //Add ActionListeners of Button1
-                button1.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent ae) {
-                        //Button1 will perform next actions
-                        if(ae.getSource() == button1)
-                        {
-                            try
-                            {
-                                Runtime.getRuntime().exec("notepad.exe");
-                            }
-                            catch (Exception r){}
-                        }
-                        // СМОТРИ СЮДА http://forum.sources.ru/index.php?showtopic=133192&view=showall
-                    }
-                });
-
-                //Add KeyListener for Button1 press emulation by pressing Num1 button
-                KeyListener listener = new KeyAdapter() {
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                        switch (e.getKeyCode()) {
-                            case KeyEvent.VK_1:
-                                button1.doClick();
-                                break;
-
-                            case KeyEvent.VK_2:
-                                button2.doClick();
-                                break;
-
-                        }
-                    }
-                };
-
-                button1.addKeyListener(listener);
-
-                this.addKeyListener(listener);
-
-                button2.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent ae) {
-                        //Button2 will perform next actions
-                        if(ae.getSource() == button2)
-                        {
-                            try
-                            {
-                                Runtime.getRuntime().exec("C:\\Users\\Sergey\\Desktop\\exr.bat");
-                            }
-                            catch (Exception r){}
-                        }
-                    }
-                });
-
-
-
-            }
-            //class MyKey implements KeyListener{
-            //    public void keyPressed(KeyEvent event){
-            //        if (event.getKeyCode() == KeyEvent.VK_ENTER){
-            //            //обработка нажатия ENTER
-            //           ;
-            //        }
-            //    }
-            //   public void keyReleased(KeyEvent event){
-            //   }
-            //   public void keyTyped(KeyEvent event){
-
-            //   }
-            //}
-
-
+        //Creating an array of statusLabels
+        statusLabel = new JLabel[quantity];
+        for (int i = 0; i < quantity; i++) {
+            statusLabel[i] = new JLabel("                      Start Script                      ");
+            statusLabel[i].setFont(labelFont);
+            statusLabel[i].setVisible(true);
+            statusLabel[i].setSize(313, 110);
+            statusLabel[i].setHorizontalAlignment(2);
+            statusLabel[i].setLocation(200, 200);
         }
 
+        //Adding timer execution
+        final TimerTick tm = new TimerTick();
+        tm.setStatusLabel(statusLabel);
 
+        //Adding Common timer to our tool,
+        timer = new Timer(1000, tm);
+        tm.setTimer(timer);
 
-        class Link2 extends JPanel{
+        //Adding all tabs to tabbed pane
+        tabbedPane.addTab("AppDataLogs", new AppDataLogs(timer, tm, font, statusLabel));
+        tabbedPane.addTab("Application", new Application(timer, tm, font, statusLabel));
+        tabbedPane.addTab("Dumps", new Dumps(timer, tm, font, statusLabel));
+        tabbedPane.addTab("Execute", new Execute(timer, tm, font, statusLabel));
+        tabbedPane.addTab("Installation", new Installation(timer, tm, font, statusLabel));
+        tabbedPane.addTab("Links", new Links(timer, tm, font, statusLabel));
+        tabbedPane.addTab("Network", new Network(timer, tm, font, statusLabel));
+        tabbedPane.addTab("Registry", new Registry(timer, tm, font, statusLabel));
+        tabbedPane.addTab("Services", new Services(timer, tm, font, statusLabel));
 
-            Link2 () {
-                final JButton button1 = new JButton("Open Squash");
-
-                button1.setFont(font);
-                button1.setVisible(true);
-                button1.setSize(313, 110);
-                add(button1);
-
-                JButton button2 = new JButton("Open Appdata");
-                button2.setFont(font);
-                button2.setVisible(true);
-                button2.setSize(313, 110);
-                add(button2);
-
-                button1.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent ae) {
-                        //Button1 will perform next actions
-                        if(ae.getSource() == button1)
-                        {
-                            try
-                            {
-                                Runtime.getRuntime().exec("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe https://git.devx.dp.ua");
-                                //    Runtime.getRuntime().exec("C:\\Program Files\\Internet Explorer\\iexplore.exe  https://git.devx.dp.ua");
-                                //     Runtime.getRuntime().exec("C:\\Windows\\System32\\cmd.exe"  );
-                                //     System.out.print("start C:\\Users\\Sergey\\Desktop\\sys");
-                            }
-                            catch (Exception r){}
-                        }
-                        // СМОТРИ СЮДА http://forum.sources.ru/index.php?showtopic=133192&view=showall
-                    }
-                });
-
-                //Add KeyListener for Button1 press emulation by pressing Num1 button
-                KeyListener listener2 = new KeyAdapter() {
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                        switch (e.getKeyCode()) {
-                            case KeyEvent.VK_1:
-                                button1.doClick();
-                                break;
-
-                            case KeyEvent.VK_2:
-                                button2.doClick();
-                                break;
-
-                        }
-                    }
-                };
-
-                button1.addKeyListener(listener2);
-
-                this.addKeyListener(listener2);
-
-            }
-
-        }
-
-        tabbedPane.addTab("*.bat exec",new Exec1(){
-        });
-
-        tabbedPane.addTab("Links",new Link2() { //REFACTOR COMPONENT TO NESTED CLASS LINKS
-        });
-
-        tabbedPane.addTab("Articles",new Component(){
-
-        });
-
-        setPreferredSize(new Dimension(260, 220));
+        //Adding tabbed pane to jFrame and Setting preferences
+        setPreferredSize(new Dimension(655, 215));
         pack();
         setLocationRelativeTo(null);
+        setResizable(false);
         setVisible(true);
         add(tabbedPane);
+    }
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/runner/TimerTick.java
@@ -0,0 +1,41 @@
+package runner;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+@Setter
+@Getter
+public class TimerTick implements ActionListener {
+
+    private Timer timer;
+    private int countdown;
+    private JLabel[] statusLabel;
+    private static final String  STATUS_LABEL_CAPTION_GAP = "                                ";
+
+    //Setting time remaining in all Labels on all Tabs
+    public void actionPerformed(ActionEvent e) {
+        countdown--;
+        showTextOnAllLabels( "Wait time " + countdown);
+        if (countdown == 0) {
+            timer.stop();
+            showTextOnAllLabels("Start Script");
+        }
+    }
+
+    //method for displaying exception to failed script execution on all tabs
+    public void showException() {
+        timer.stop();
+        showTextOnAllLabels("Script starting failed. Try again");
+    }
+
+    //Private method to setting text on all tabs
+    private void showTextOnAllLabels(String text) {
+        for (JLabel statusLabel : statusLabel) {
+            statusLabel.setText(STATUS_LABEL_CAPTION_GAP + text  + STATUS_LABEL_CAPTION_GAP);
+        }
+    }
+}
