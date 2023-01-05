cb35ad0e39b6526ad1b3837fe51e407513db17a1
SergeyShidlovsky <shedlovskys@gmail.com>
2023-01-06 00:49:05 +0300

    Fixed Services tab

--- src/main/java/tabs/Services.java
+++ src/main/java/tabs/Services.java
@@ -10,67 +10,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 
-public class Services extends JPanel {
+public class Services extends AbstractTab {
 
     final JButton button1;
     final JButton button2;
     final JButton button3;
     final JButton button4;
     final JButton button5;
-    private JLabel tabStatusLabel;
-    private KeyListener listener8;
-    private Map<Integer, JButton> buttonAssignment;
 
-    //Adding method for reset timer with new value
-    // after buttons below have been pressed
-    public void timeReset(int seconds, Timer timer, TimerTick tm) {
-        timer.stop();
-        tm.setCountdown(seconds);
-        timer.start();
-    }
-
-    private void addActionListenerToButton(final JButton button, final String command, final Timer timer,
-                                           final TimerTick tm, final int delay) {
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                //Button1 will perform next actions
-                if (ae.getSource() == button) {
-                    try {
-                        Runtime.getRuntime().exec(command);
-                        timeReset(delay, timer, tm);
-                    } catch (Exception r) {
-                        tm.showError();
-                    }
-                }
-            }
-        });
-    }
-
-    private void addButtonWithPreferencesToTab(final JButton button, String tooltip, Font font, int keyCode) {
-        button.setFont(font);
-        button.setVisible(true);
-        button.setToolTipText(tooltip);
-        button.setSize(313, 110);
-        add(button);
-        buttonAssignment.put(keyCode, button);
-    }
-
-    private KeyListener addKeyListenerToTab(){
-        return new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                Integer currentAssignmentCode;
-                for (int i = 0; i < buttonAssignment.values().size(); i++) {
-                    currentAssignmentCode = (Integer) buttonAssignment.keySet().toArray()[i];
-                    if (e.getKeyCode() == currentAssignmentCode) {
-                        buttonAssignment.get(currentAssignmentCode).doClick();
-                    }
-                }
-            }
-        };
-    }
-
-    public Services(Timer timer, TimerTick tm, Font font, List<JLabel> statusLabelList) {
+    public Services(TimerTick tm, Font font, List<JLabel> statusLabelList) {
 
         //Create list of buttons
         button1 = new JButton("Open Services [1]");
@@ -90,21 +38,17 @@ public class Services extends JPanel {
         addButtonWithPreferencesToTab(button5, "Stop Integration Service", font, KeyEvent.VK_5);
 
         //Add ActionListeners on all buttons
-        addActionListenerToButton(button1, LinksServices.OPEN_SERVICE.getValue(), timer, tm, 2);
-        addActionListenerToButton(button2, LinksServices.START_AUDIO_SERVICE.getValue(), timer, tm, 3);
-        addActionListenerToButton(button3, LinksServices.START_INTEGRATION_SERVICE.getValue(), timer, tm, 4);
-        addActionListenerToButton(button4, LinksServices.STOP_AUDIO_SERVICE.getValue(), timer, tm, 3);
-        addActionListenerToButton(button5, LinksServices.STOP_INTEGRATION_SERVICE.getValue(), timer, tm, 5);
+        addActionListenerToButton(button1, LinksServices.OPEN_SERVICE.getValue(), tm, 2);
+        addActionListenerToButton(button2, LinksServices.START_AUDIO_SERVICE.getValue(), tm, 3);
+        addActionListenerToButton(button3, LinksServices.START_INTEGRATION_SERVICE.getValue(), tm, 4);
+        addActionListenerToButton(button4, LinksServices.STOP_AUDIO_SERVICE.getValue(), tm, 3);
+        addActionListenerToButton(button5, LinksServices.STOP_INTEGRATION_SERVICE.getValue(), tm, 5);
 
         //Create tab-specific KeyListener
-        listener8 = addKeyListenerToTab();
+        listener = addKeyListenerToTab();
 
         //Add key Listeners to all Buttons
-        button1.addKeyListener(listener8);
-        button2.addKeyListener(listener8);
-        button3.addKeyListener(listener8);
-        button4.addKeyListener(listener8);
-        button5.addKeyListener(listener8);
+        buttonAssignment.values().forEach(button -> button.addKeyListener(listener));
 
         //Add StatusLabel to tab
         tabStatusLabel = statusLabelList.get(8);
